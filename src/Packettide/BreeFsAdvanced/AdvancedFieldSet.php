57b19ce90f4daca5bb2a52e03ded9ebfcb7f066d
Bryan Nielsen <bryan@packettide.com>
2013-10-23 17:13:12 -0400

    initial commit

---
+++ src/Packettide/BreeFsAdvanced/AdvancedFieldSet.php
@@ -0,0 +1,14 @@
+<?php namespace Packettide\BreeFsAdvanced;
+
+use Packettide\Bree\FieldSet;
+
+class AdvancedFieldSet extends FieldSet {
+
+	public $assets = array(
+			'packettide/bree-fs-advanced/jquery.min.js',
+			'packettide/bree-fs-advanced/handlebars.js'
+		);
+
+	public $name = 'advanced';
+
+}
---
+++ src/Packettide/BreeFsAdvanced/BreeFsAdvancedServiceProvider.php
@@ -0,0 +1,45 @@
+<?php namespace Packettide\BreeFsAdvanced;
+
+use Packettide\Bree\FieldSetProvider as FieldSetProvider;
+use Illuminate\Support\ServiceProvider;
+
+class BreeFsAdvancedServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('packettide/bree-fs-advanced');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
